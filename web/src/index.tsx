efa88151aedc1aa636af6ec9f927e7f3248cad56
keychi25 <m208125n@st.u-gakugei.ac.jp>
2022-07-17 13:45:02 +0900

    add: apllloclent
    - ApolloClientの設定
    - Booksの取得

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const link = createHttpLink({
+  uri: 'http://localhost:8000/graphql',
+  credentials: 'include'
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache,
+  link: link
+})
+
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
