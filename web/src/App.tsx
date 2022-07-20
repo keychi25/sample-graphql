2cf83964b9377dd158318ea6b868cc03637be065
keychi25 <m208125n@st.u-gakugei.ac.jp>
2022-07-20 20:31:16 +0900

    add: createBook

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,19 @@
-import { useBooksQuery } from "./graphql/generated";
-
+import { useBooksQuery, useCreateBookMutation } from "./graphql/generated";
+import { useState } from "react"
+import { setSyntheticTrailingComments } from "typescript";
 function App() {
   const { data: { books = [] } = {} } = useBooksQuery();
-
+  const [createBook] = useCreateBookMutation({refetchQueries: ["Books"]});
+  const [title, setTitle] =  useState("")
   return (
     <div>
+      <input value={title} onChange={(e) => setTitle(e.target.value) } />
+      <button onClick={() => {
+        createBook({variables: {params: {title: title}}});
+        setTitle(""); // 初期化
+      }}>
+        保存
+      </button>
       {books.map((book) => (
         <div key={book.id}>{book.title}</div>
       ))}
